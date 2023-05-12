bf389f1c97afd27116247bd1906625ce15018ebb
MikeChamp4 <zambranocmiguele@gmail.com>
2023-05-12 10:44:28 +0200

    Metodo sumarValorAbsoluto

--- src/main/java/mzm/m5_uf2_examen/M5_UF2_Examen.java
+++ src/main/java/mzm/m5_uf2_examen/M5_UF2_Examen.java
@@ -2,7 +2,12 @@ package mzm.m5_uf2_examen;
 
 public class M5_UF2_Examen {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private int num1;
+    private int num2;
+
+    private int sumarValorAbsolut(int num1, int num2){
+        int vAbsoluto = Math.abs(num1) +  Math.abs(num2);
+        return vAbsoluto;
     }
+
 }
