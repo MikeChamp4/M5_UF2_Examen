ca9c83a3af370156c457b4520b443e623f5367e4
MikeChamp4 <zambranocmiguele@gmail.com>
2023-05-12 10:45:06 +0200

    Metodo Sumar

--- src/main/java/mzm/m5_uf2_examen/M5_UF2_Examen.java
+++ src/main/java/mzm/m5_uf2_examen/M5_UF2_Examen.java
@@ -5,7 +5,7 @@ public class M5_UF2_Examen {
     private int num1;
     private int num2;
     
-    public int sumar() {
+    public int sumar(int num1, int num2) {
         return num1 + num2;
     }
     
