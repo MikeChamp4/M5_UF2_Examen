c68cbb0b1880d50eebf99ea4373f9dcbba007022
MikeChamp4 <zambranocmiguele@gmail.com>
2023-05-12 10:38:46 +0200

    Metodo getNum1

--- src/main/java/mzm/m5_uf2_examen/M5_UF2_Examen.java
+++ src/main/java/mzm/m5_uf2_examen/M5_UF2_Examen.java
@@ -2,7 +2,11 @@ package mzm.m5_uf2_examen;
 
 public class M5_UF2_Examen {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private int num1;
+    private int num2;
+
+    public int getNum1(){
+        return num1;
     }
 }
+        
